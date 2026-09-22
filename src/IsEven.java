f49735a30bd50be80da9845b698e7bc2d79644e5
sprycr <sprycr@mail.uc.edu>
2026-09-22 00:08:34 -0400

    Completed task 4

--- src/IsEven.java
+++ src/IsEven.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class IsEven {
+    public static void main(String[] args) {
+        int numToExamine = 3;
+        double result = numToExamine % 2;
+
+        System.out.println("the number is(1 is odd, 0 is even): " + result);
+    }
 }
